46b17edb88cb374b22206a854beb4df922aa704c
Navindya Madhuhansi <86855178+Navindya99@users.noreply.github.com>
2023-05-10 02:14:54 +0530

    Added files

--- app/src/main/java/com/example/eyetravel/user/UpdateCardDetailsActivity.kt
+++ app/src/main/java/com/example/eyetravel/user/UpdateCardDetailsActivity.kt
@@ -3,54 +3,52 @@ package com.example.eyetravel.user
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.example.eyetravel.R
 import com.example.eyetravel.models.CardModel
 import com.example.eyetravel.models.PaymentModel
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import kotlin.math.exp
 
-class CardDetailsActivity : AppCompatActivity() {
+class UpdateCardDetailsActivity : AppCompatActivity() {
     private lateinit var dbRef: DatabaseReference
-
     private lateinit var etCardNumber: EditText
     private lateinit var etExpiryDate: EditText
     private lateinit var etCode: EditText
 
-    private lateinit var cardId: String
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_card_details)
+        setContentView(R.layout.activity_update_card_details)
 
-        dbRef = FirebaseDatabase.getInstance().getReference("Cards")
-
-        etCardNumber = findViewById(R.id.add_card_number)
-        etExpiryDate = findViewById(R.id.add_card_expiry_date)
-        etCode = findViewById(R.id.add_card_code)
+        etCardNumber = findViewById(R.id.update_card_number)
+        etExpiryDate = findViewById(R.id.update_card_expiry_date)
+        etCode = findViewById(R.id.update_card_code)
 
         val paymentId = intent.getStringExtra("paymentId")
 
-        val payNow = findViewById<Button>(R.id.btn_pay_now)
-        payNow.setOnClickListener{
-            saveCard()
+        setValuesToViews()
+
+        val btnPay = findViewById<Button>(R.id.update_card_pay)
+        btnPay.setOnClickListener{
+            updateCard()
             finish()
-            val intent = Intent(this, ViewCardDetailsActivity::class.java)
+            val intent = Intent(this, PaymentSuccessfulActivity::class.java)
             intent.putExtra("paymentId", paymentId)
-
-            intent.putExtra("cardId", cardId)
-            intent.putExtra("cardNumber", etCardNumber.text.toString())
-            intent.putExtra("expiryDate", etExpiryDate.text.toString())
-            intent.putExtra("code", etCode.text.toString())
-
             startActivity(intent)
         }
     }
 
-    private fun saveCard(){
+    private fun setValuesToViews() {
+        etCardNumber.text = Editable.Factory.getInstance().newEditable(intent.getStringExtra("cardNumber"))
+        etExpiryDate.text = Editable.Factory.getInstance().newEditable(intent.getStringExtra("expiryDate"))
+        etCode.text = Editable.Factory.getInstance().newEditable(intent.getStringExtra("code"))
+    }
+
+    private fun updateCard() {
         val cardNumber = etCardNumber.text.toString()
         val expiryDate = etExpiryDate.text.toString()
         val code = etCode.text.toString()
@@ -58,15 +56,10 @@ class CardDetailsActivity : AppCompatActivity() {
         if (cardNumber == "" || expiryDate == "" || code == ""){
             Toast.makeText(this, "All fields required", Toast.LENGTH_LONG).show()
         }else{
-            cardId = dbRef.push().key!!
+            val cardId = intent.getStringExtra("cardId")
+            dbRef = FirebaseDatabase.getInstance().getReference("Cards/$cardId")
             val cardModel = CardModel(cardId, cardNumber, expiryDate, code)
-            dbRef.child(cardId).setValue(cardModel).addOnCompleteListener{
-                Toast.makeText(this, "Card Details Saved", Toast.LENGTH_LONG).show()
-            }.addOnFailureListener { err->
-                Toast.makeText(this, "Error ${err.message} ", Toast.LENGTH_LONG).show()
-            }
+            dbRef.setValue(cardModel)
         }
-
-
     }
 }
